306e88d4650f75237790ca28b873a72367387791
nikita <larin_nikita_m@mail.ru>
2023-02-18 16:53:58 +0700

    login-form

--- js/main.js
+++ js/main.js
@@ -35,29 +35,88 @@ toggle.onclick = function () {
     sidebar.classList.toggle("close");
 };
 
-var editors = [];
+// var editors = [];
 
-$('.text-editor').each(function (idx, wrapper) {
-    var e = new wysihtml.Editor($(wrapper).find('.editable').get(0), {
-        toolbar: $(wrapper).find('.toolbar').get(0),
-        parserRules: wysihtmlParserRules,
-        stylesheets: "wysihtml/examples/css/stylesheet.css"
-    });
-    editors.push(e);
+// $('.text-editor').each(function (idx, wrapper) {
+//     var e = new wysihtml.Editor($(wrapper).find('.editable').get(0), {
+//         toolbar: $(wrapper).find('.toolbar').get(0),
+//         parserRules: wysihtmlParserRules,
+//         stylesheets: "wysihtml/examples/css/stylesheet.css"
+//     });
+//     editors.push(e);
+// });
+
+// function previewFile() {
+//     var preview = document.querySelector('.preview');
+//     var file = document.querySelector('input[type=file]').files[0];
+//     var reader = new FileReader();
+
+//     reader.onloadend = function () {
+//         preview.src = reader.result;
+//     }
+
+//     if (file) {
+//         reader.readAsDataURL(file);
+//     } else {
+//         preview.src = "";
+//     }
+// }
+
+var button = document.getElementById('mainButton');
+
+var openForm = function() {
+	button.className = 'active';
+    body.style.overflow = "hidden";
+    body.style.pointerEvents = "none";
+    document.querySelector(".modal").style.pointerEvents = "auto";
+    body.style.userSelect ="none";
+};
+
+var checkInput = function(input) {
+	if (input.value.length > 0) {
+		input.className = 'active';
+        body.style.overflow = "hidden";
+        body.style.pointerEvents = "none";
+        document.querySelector(".modal").style.pointerEvents = "auto";
+        body.style.userSelect ="none";
+	} else {
+		input.className = '';
+        body.style.overflow = "auto";
+        body.style.pointerEvents = "auto";
+        body.style.userSelect ="auto";
+	}
+};
+
+var closeForm = function() {
+	button.className = '';
+    body.style.overflow = "auto";
+    body.style.pointerEvents = "auto";
+    body.style.userSelect ="auto";
+};
+
+document.addEventListener("keyup", function(e) {
+	if (e.keyCode == 27 || e.keyCode == 13) {
+		closeForm();
+	}
 });
 
-function previewFile() {
-    var preview = document.querySelector('.preview');
-    var file = document.querySelector('input[type=file]').files[0];
-    var reader = new FileReader();
+// function myFunction() {
+//     var x = document.getElementById("user-password");
+//     if (x.type === "password") {
+//       x.type = "text";
+//     } else {
+//       x.type = "password";
+//     }
+//   }
 
-    reader.onloadend = function () {
-        preview.src = reader.result;
-    }
-
-    if (file) {
-        reader.readAsDataURL(file);
-    } else {
-        preview.src = "";
-    }
+function show_hide_password(target){
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
 }
