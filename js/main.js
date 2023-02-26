300e78d4eedc94eca5a5cac5ecce1214bed159a1
nikita <larin_nikita_m@mail.ru>
2023-02-26 17:09:25 +0700

    ui-kit

--- js/main.js
+++ js/main.js
@@ -87,3 +87,19 @@ var closePrintForm = function () {
     PrintForm.style.display = "none";
     body.style.overflow = "auto";
 };
+
+function previewFile() {
+    var preview = document.querySelector('.preview');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+}
