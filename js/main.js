b146114fc3d85738ddafa1116c481668eb03539b
nikita <larin_nikita_m@mail.ru>
2023-03-01 16:44:12 +0700

    book

--- js/main.js
+++ js/main.js
@@ -71,11 +71,3 @@ function topFunction(){
         return false;
     }
 }
-
-$(document).ready(function () {
-    $('.js-select2').select2({
-        placeholder: "Выберите",
-        maximumSelectionLength: 2,
-        language: "ru"
-    });
-});
---
+++ js/select2.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.js-select2').select2({
+        placeholder: "Выберите",
+        maximumSelectionLength: 2,
+        language: "ru"
+    });
+});
